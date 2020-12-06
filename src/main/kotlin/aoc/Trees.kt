7ca9a302720b7809fb527a1499ed73c1a5598f1b
Matt Thompson <matt.thompson@johnlewis.co.uk>
2020-12-06 16:09:10 +0000

    Add day 3 part 2

--- src/main/kotlin/aoc/Trees.kt
+++ src/main/kotlin/aoc/Trees.kt
@@ -1,17 +1,20 @@
 package aoc
 
+import kotlin.math.ceil
+
 fun treesEncountered(input: List<String>, gradient: Double): Int =
     encounters(makeMapCorrectWidth(input, gradient), gradient)
+        .filterNot { it.isEmpty() }
         .filter { it.single() == '#' }
         .size
 
 fun makeMapCorrectWidth(treeMap: List<String>, gradient: Double): List<String> {
     val width = treeMap[0].length - 1
     val length = treeMap.size - 1
-    val repeats = (length * gradient) / width
+    val repeats = (length.toDouble() * gradient) / width.toDouble()
 
     return treeMap.map {
-        it.repeat(if (repeats <= 1) 1 else repeats.toInt())
+        it.repeat(if (repeats <= 1) 1 else ceil(repeats).toInt())
     }
 }
 
@@ -21,5 +24,3 @@ fun encounters(treeMap: List<String>, gradient: Double): List<String> =
            columnIndex.toDouble() == rowIndex * gradient
        }
     }.drop(1)
-
-
--- src/test/kotlin/aoc/TreesKtTest.kt
+++ src/test/kotlin/aoc/TreesKtTest.kt
@@ -2,7 +2,6 @@ package aoc
 
 import assertk.assertThat
 import assertk.assertions.containsExactly
-import assertk.assertions.containsOnly
 import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
@@ -42,19 +41,7 @@ class TreesKtTest {
 
     @Test
     fun `should return 7 trees encountered`() {
-        val input = listOf(
-            "..##.........##.........##.........##.........##.........##.......",
-            "#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..",
-            ".#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.",
-            "..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#",
-            ".#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.",
-            "..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....",
-            ".#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#",
-            ".#........#.#........#.#........#.#........#.#........#.#........#",
-            "#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...",
-            "#...##....##...##....##...##....##...##....##...##....##...##....#",
-            ".#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#"
-        )
+        val input = exampleInput
 
         val result = treesEncountered(input, 3.0)
 
@@ -68,5 +55,57 @@ class TreesKtTest {
         println(result)
 
         assertThat(result).isEqualTo(252)
+    }
+
+    // Part 2
+
+    @Test
+    fun `should return trees encountered for various gradients for example input`() {
+        val input = exampleInput
+
+        val resultForGradientOf1 = treesEncountered(input, 1.0)
+        val resultForGradientOf3 = treesEncountered(input, 3.0)
+        val resultForGradientOf5 = treesEncountered(input, 5.0)
+        val resultForGradientOf7 = treesEncountered(input, 7.0)
+        val resultForGradientOfHalf = treesEncountered(input, 0.5)
+
+        assertThat(resultForGradientOf1).isEqualTo(2)
+        assertThat(resultForGradientOf3).isEqualTo(7)
+        assertThat(resultForGradientOf5).isEqualTo(3)
+        assertThat(resultForGradientOf7).isEqualTo(4)
+        assertThat(resultForGradientOfHalf).isEqualTo(2)
+    }
+
+    @Test
+    fun `should return trees encountered for various gradients for full input and multiply together`() {
+        val input = treeMapInput
+
+        val resultForGradientOf1 = treesEncountered(input, 1.0)
+        val resultForGradientOf3 = treesEncountered(input, 3.0)
+        val resultForGradientOf5 = treesEncountered(input, 5.0)
+        val resultForGradientOf7 = treesEncountered(input, 7.0)
+        val resultForGradientOfHalf = treesEncountered(input, 0.5)
+
+        val resultList = listOf(
+            resultForGradientOf1.toLong(),
+            resultForGradientOf3.toLong(),
+            resultForGradientOf5.toLong(),
+            resultForGradientOf7.toLong(),
+            resultForGradientOfHalf.toLong()
+        )
+
+        println("list: $resultList")
+
+        assertThat(resultForGradientOf1).isEqualTo(57)
+        assertThat(resultForGradientOf3).isEqualTo(252)
+        assertThat(resultForGradientOf5).isEqualTo(64)
+        assertThat(resultForGradientOf7).isEqualTo(66)
+        assertThat(resultForGradientOfHalf).isEqualTo(43)
+
+        val productOfResults = resultList.reduce { total, next -> total * next }
+
+        println("product: $productOfResults")
+
+        assertThat(productOfResults).isEqualTo(2608962048)
     }
 }
