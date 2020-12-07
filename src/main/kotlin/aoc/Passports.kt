c7c2666bbfdd2d426e01388ab0eb6d3631044769
Matt Thompson <matt.thompson@johnlewis.co.uk>
2020-12-07 09:44:14 +0000

    Add day 4 part 2

--- src/main/kotlin/aoc/Passports.kt
+++ src/main/kotlin/aoc/Passports.kt
@@ -2,17 +2,52 @@ package aoc
 
 fun countValidPassports(batchInput: String): Int =
     separatePassports(batchInput)
-        .filter { isPassportValid(it) }.size
+        .filter { isPassportComplete(it) }.size
 
 fun separatePassports(batchInput: String): List<String> =
     batchInput.split("\n\n")
 
-fun isPassportValid(input: String): Boolean {
+fun isPassportComplete(input: String): Boolean {
     val fieldsPresent = input
         .split(" ", "\n")
         .map { it.substringBefore(':') }
 
     return fieldsPresent.containsAll(fields)
+}
+
+// part 2
+
+fun countValidAndCompletePassports(batchInput: String): Int =
+    separatePassports(batchInput)
+        .filter { isPassportComplete(it) }
+        .filter { areFieldsValid(it) }.size
+
+fun areFieldsValid(input: String): Boolean {
+    val fieldsAndValues = input
+        .split(" ", "\n")
+        .map {
+            Pair(it.substringBefore(":"), it.substringAfter(":"))
+        }
+
+    val fieldValidations = fieldsAndValues.map { isFieldValid(it) }
+
+    return !fieldValidations.contains(false)
+}
+
+fun isFieldValid(fieldAndValue: Pair<String, String>): Boolean {
+    val field = fieldAndValue.first
+    val value = fieldAndValue.second
+
+    return when (field) {
+        "ecl" -> listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").any { it == value }
+        "pid" -> value.length == 9
+        "eyr" -> value.length == 4 && value.toInt() in 2020..2030
+        "hcl" -> value.first() == '#' && value.length == 7 && Regex("([a-f]|[0-9]){6}").matches(value.drop(1))
+        "byr" -> value.length == 4 && value.toInt() in 1920..2002
+        "iyr" -> value.length == 4 && value.toInt() in 2010..2020
+        "hgt" -> validateHeight(value)
+        else -> true
+    }
 }
 
 val fields = listOf(
@@ -24,3 +59,18 @@ val fields = listOf(
     "iyr",
     "hgt"
 )
+
+private fun validateHeight(value: String): Boolean {
+    val unit = value.takeLast(2)
+    val measurement = value.take(value.length - 2)
+    val unitIsValid = unit == "cm" || unit == "in"
+    val measurementIsValid =
+        when (unit) {
+            "cm" -> measurement.toInt() in 150..193
+            "in" -> measurement.toInt() in 59..76
+            else -> false
+        }
+
+    return unitIsValid && measurementIsValid
+}
+
--- src/test/kotlin/aoc/PassportsKtTest.kt
+++ src/test/kotlin/aoc/PassportsKtTest.kt
@@ -23,7 +23,7 @@ class PassportsKtTest {
             byr:1937 iyr:2017 cid:147 hgt:183cm
         """.trimIndent()
 
-        val result = isPassportValid(input)
+        val result = isPassportComplete(input)
 
         assertThat(result).isTrue()
     }
@@ -35,7 +35,7 @@ class PassportsKtTest {
             hcl:#cfa07d byr:1929
         """.trimIndent()
 
-        val result = isPassportValid(input)
+        val result = isPassportComplete(input)
 
         assertThat(result).isFalse()
     }
@@ -49,7 +49,7 @@ class PassportsKtTest {
             hgt:179cm
         """.trimIndent()
 
-        val result = isPassportValid(input)
+        val result = isPassportComplete(input)
 
         assertThat(result).isTrue()
     }
@@ -61,7 +61,7 @@ class PassportsKtTest {
             iyr:2011 ecl:brn hgt:59in
         """.trimIndent()
 
-        val result = isPassportValid(input)
+        val result = isPassportComplete(input)
 
         assertThat(result).isFalse()
     }
@@ -80,5 +80,47 @@ class PassportsKtTest {
         println(result)
 
         assertThat(result).isEqualTo(264)
+    }
+
+    // part 2
+
+    @Test
+    fun `should validate full set of individual fields`() {
+        val input = """
+            ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+            byr:1937 iyr:2017 cid:147 hgt:183cm
+        """.trimIndent()
+
+        val result = areFieldsValid(input)
+
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `should validate individual fields`() {
+        val eclResult = isFieldValid(Pair("ecl", "gry"))
+        val pidResult = isFieldValid(Pair("pid", "860033327"))
+        val eyrResult = isFieldValid(Pair("eyr", "2020"))
+        val hclResult = isFieldValid(Pair("hcl", "#fffffd"))
+        val byrResult = isFieldValid(Pair("byr", "1937"))
+        val iyrResult = isFieldValid(Pair("iyr", "2017"))
+        val hgtResult = isFieldValid(Pair("hgt", "183cm"))
+
+        assertThat(eclResult).isTrue()
+        assertThat(pidResult).isTrue()
+        assertThat(eyrResult).isTrue()
+        assertThat(hclResult).isTrue()
+        assertThat(byrResult).isTrue()
+        assertThat(iyrResult).isTrue()
+        assertThat(hgtResult).isTrue()
+    }
+
+    @Test
+    fun `should count all valid and complete passports for full batch input`() {
+        val result = countValidAndCompletePassports(passportBatchInput)
+
+        println(result)
+
+        assertThat(result).isEqualTo(224)
     }
 }
