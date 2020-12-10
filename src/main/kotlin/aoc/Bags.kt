340e92c03d01a706db037173333297d210836317
Matt Thompson <matt.thompson@johnlewis.co.uk>
2020-12-10 14:22:06 +0000

    WIP tree structure day 7 part 2

--- src/main/kotlin/aoc/Bags.kt
+++ src/main/kotlin/aoc/Bags.kt
@@ -1,52 +1,50 @@
 package aoc
 
-fun extractContainingBagColours(bagColours: List<String>, rule: String): String? =
-    if (bagColours.map { rule.substringAfter("contain").contains(it) }.any { it }) {
-        rule.substringBefore(" bag")
-    } else {
-        null
+fun countBagsInTree(tree: List<Bag>): Int =
+    tree.map {
+        if (it.children.isNotEmpty()) {
+            it.number + (it.number * countBagsInTree(it.children))
+        } else {
+            it.number
+        }
+    }.reduce { total, next -> total + next }
+
+fun createTreeFrom(startingBag: Pair<Int, String>, rules: List<String>): List<Bag> {
+    val startingRule = rules.single {
+        it.substringBefore(" bags contain") == startingBag.second
     }
 
-fun bagsContaining(bagColours: List<String>, rules: List<String>, count: Int = 0): Int {
-    val directlyContainingBags = rules
-        .mapNotNull { extractContainingBagColours(bagColours, it) }
-        .distinct()
-
-    val reducedRules = rules.filterNot { rule -> // if any of the bagColours are contained in subStringAfter("contain")
-        bagColours.map { rule.substringAfter("contain").contains(it) }.any { it }
+    return numbersAndColoursFrom(startingRule).map {
+        Bag(
+            number = it.first,
+            colour = it.second,
+            children = if (!hasChildren(it.second, rules)) {
+                emptyList()
+            } else {
+                createTreeFrom(it, rules)
+            }
+        )
     }
-
-    return if (directlyContainingBags.isNullOrEmpty()) count
-    else bagsContaining(directlyContainingBags, reducedRules, directlyContainingBags.size + count)
 }
 
-// part 2
-
-fun bagsInside(outerBagColours: List<String>, rules: List<String>, count: Int = 0): Int {
-    val directRules = outerBagColours.flatMap { colour ->
-        rules.filter { rule ->
-            rule.substringBefore(" bags contain") == colour
-        }
-    }
-
-    return if (directRules.isEmpty()) {
-        count
-    } else {
-        val containedNumbersOfEachColourBag = directRules.flatMap { directRule ->
-            val containedBags =
-                directRule.substringAfter("contain ").split(", ") // listOf("1 dark olive bag", "2 vibrant plum bags")
-            containedBags.map {
-                Pair(it.take(1).toInt(), it.drop(2).substringBefore(" bag")) // Pair(1, "dark olive")
+fun numbersAndColoursFrom(rule: String): List<Pair<Int, String>> =
+    rule.substringAfter("contain ")
+        .split(", ")
+        .map {
+            if (it.take(1) == "n") {
+                Pair(0, it.drop(3).substringAfter(" bag"))
+            } else {
+                Pair(it.take(1).toInt(), it.drop(2).substringBefore(" bag"))
             }
         }
 
-        val thisCount = containedNumbersOfEachColourBag
-            .map { it.first }
-            .reduce { total, next -> total + next }
+private fun hasChildren(bagColour: String, rules: List<String>): Boolean =
+    rules.single { it.substringBefore(" bags contain") == bagColour }
+        .substringAfter("contain ")
+        .take(2) != "no"
 
-        val listOfNextColours = containedNumbersOfEachColourBag.map { it.second }
-
-        return if (thisCount == 0) count
-        else bagsInside(listOfNextColours, rules, thisCount + count)
-    }
-}
+data class Bag(
+    val number: Int,
+    val colour: String,
+    val children: List<Bag>
+)
--- src/test/kotlin/aoc/BagsKtTest.kt
+++ src/test/kotlin/aoc/BagsKtTest.kt
@@ -6,94 +6,148 @@ import assertk.assertions.isNull
 import org.junit.jupiter.api.Test
 
 class BagsKtTest {
-    @Test
-    fun `should extract bags containing shiny gold bag`() {
-        val input = "bright white bags contain 1 shiny gold bag"
+    // tree structure attempt
 
-        val result = extractContainingBagColours(listOf("shiny gold"), input)
-
-        assertThat(result).isEqualTo("bright white")
-    }
-
-    @Test
-    fun `should extract no bags when rule does not contain desired bag colour`() {
-        val input = "bright white bags contain 1 shiny gold bag"
-
-        val result = extractContainingBagColours(listOf("muted yellow"), input)
-
-        assertThat(result).isNull()
-    }
+    // think a method needs to traverse all the way to the bottom of the rules first...
+    // for the 48 test above
+    // shiny gold -> 1 dark olive   -> 3 faded blue   -> 0 =                 (1) + (1*3) + (1*3*0) = 4
+    //                              -> 4 dotted black -> 1 faded blue -> 0 =       (1*4) + (1*4*1) + (1*4*1*0) = 8
+    //            -> 2 vibrant plum -> 5 faded blue   -> 0 =                 (2) + (2*5) + (2*5*0) = 12
+    //                              -> 6 dotted black -> 1 faded blue -> 0 =       (2*6) + (2*6*1) + (2*6*1*0) = 24
+    //                                                                     = 48
 
     @Test
-    fun `should return 1 bag colour that contains one shiny gold bag`() {
-        val input = listOf("bright white bags contain 1 shiny gold bag")
-
-        val result = bagsContaining(listOf("shiny gold"), input)
-
-        assertThat(result).isEqualTo(1)
-    }
-
-    @Test
-    fun `should return 4 bag colours that can contain shiny gold bags`() {
-        val input = exampleBagRules
-
-        val result = bagsContaining(listOf("shiny gold"), input)
-
-        assertThat(result).isEqualTo(4)
-    }
-
-    @Test
-    fun `should return all bag colours that can contain shiny gold bags from full list`() {
-        val input = bagRules
-
-        val result = bagsContaining(listOf("shiny gold"), input)
-
-        println(result)
-
-        assertThat(result).isEqualTo(287)
-    }
-
-    // part 2
-
-    @Test
-    fun `should return 0 for number of bags faded blue bags should contain`() {
-        val input = listOf("faded blue bags contain 0 other bags")
-
-        val result = bagsInside(listOf("faded blue"), input)
-
-        assertThat(result).isEqualTo(0)
-    }
-
-    @Test
-    fun `should return 3 for number of bags shiny gold bags should contain`() {
-        val input = listOf("shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags")
-
-        val result = bagsInside(listOf("shiny gold"), input)
-
-        assertThat(result).isEqualTo(3)
-    }
-
-    @Test
-    fun `should return 6 for number of bags shiny gold bags should contain`() {
-        val input = listOf(
+    fun `should create tree structure from rule data`() {
+        val rules = listOf(
             "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags",
-            "dark olive bags contain 3 faded blue bags"
+            "faded blue bags contain no other bags",
+            "dotted black bags contain 1 faded blue bags",
+            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags",
+            "dark olive bags contain 3 faded blue bags, 4 dotted black bags"
         )
 
-        val result = bagsInside(listOf("shiny gold"), input)
+        val result = createTreeFrom(Pair(1, "shiny gold"), rules)
 
-        assertThat(result).isEqualTo(6)
+        val expected = exampleRuleTree48Bags
+
+        assertThat(result).isEqualTo(expected)
     }
 
-//    @Test
-//    fun `should return 32 for number of bags shiny gold bags should contain`() {
-//        val input = listOf(
-//            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags",
-//            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags"
-//        )
-//
-//        val result = bagsInside(listOf("shiny gold"), input)
-//
-//        assertThat(result).isEqualTo(25)
-//    }
+    @Test
+    fun `should count 8 bags in tree`() {
+        val tree = exampleRuleTree8Bags
+
+        val result = countBagsInTree(tree)
+
+        assertThat(result).isEqualTo(8)
+    }
+
+    @Test
+    fun `should count 12 bags in tree`() {
+        val tree = exampleRuleTree12Bags
+
+        val result = countBagsInTree(tree)
+
+        assertThat(result).isEqualTo(12)
+    }
+
+    @Test
+    fun `should count 48 bags in tree`() {
+        val tree = exampleRuleTree48Bags
+
+        val result = countBagsInTree(tree)
+
+        assertThat(result).isEqualTo(48)
+    }
 }
+
+val exampleRuleTree8Bags = listOf(
+    Bag(
+        number = 1, // 1
+        colour = "dark olive",
+        children = listOf(
+            Bag(
+                number = 3, // + (1*3)
+                colour = "faded blue",
+                children = emptyList()
+            ),
+            Bag(
+                number = 4, // + (1*4)
+                colour = "dotted black",
+                children = emptyList()
+            )
+        )
+    )
+)
+
+val exampleRuleTree12Bags = listOf(
+    Bag(
+        number = 1, // 1
+        colour = "dark olive",
+        children = listOf(
+            Bag(
+                number = 3, // + (1*3)
+                colour = "faded blue",
+                children = emptyList()
+            ),
+            Bag(
+                number = 4, // + (1*4)
+                colour = "dotted black",
+                children = listOf(
+                    Bag(
+                        number = 1, // + (1*4*1)
+                        colour = "faded blue",
+                        children = emptyList()
+                    )
+                )
+            )
+        )
+    )
+)
+
+val exampleRuleTree48Bags = listOf(
+    Bag(
+        number = 1,
+        colour = "dark olive",
+        children = listOf(
+            Bag(
+                number = 3,
+                colour = "faded blue",
+                children = emptyList()
+            ),
+            Bag(
+                number = 4,
+                colour = "dotted black",
+                children = listOf(
+                    Bag(
+                        number = 1,
+                        colour = "faded blue",
+                        children = emptyList()
+                    )
+                )
+            )
+        )
+    ),
+    Bag(
+        number = 2,
+        colour = "vibrant plum",
+        children = listOf(
+            Bag(
+                number = 5,
+                colour = "faded blue",
+                children = emptyList()
+            ),
+            Bag(
+                number = 6,
+                colour = "dotted black",
+                children = listOf(
+                    Bag(
+                        number = 1,
+                        colour = "faded blue",
+                        children = emptyList()
+                    )
+                )
+            )
+        )
+    )
+)
