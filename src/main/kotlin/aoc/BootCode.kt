c8e4045f70671f812a0d6f1421333a0c35e02964
Matt Thompson <matt.thompson@johnlewis.co.uk>
2020-12-12 11:24:41 +0000

    Refactor to remove mutable acc variable and unnecessary HandheldDevice class

--- src/main/kotlin/aoc/BootCode.kt
+++ src/main/kotlin/aoc/BootCode.kt
@@ -1,45 +1,48 @@
 package aoc
 
-class HandheldDevice {
-    var acc = 0
-
-    fun runProgram(
-        instructions: List<String>,
-        index: Int = 0,
-        indexesVisited: List<Int> = emptyList()
-    ): Int {
-        return if (indexesVisited.contains(index)) {
-            acc
-        } else {
-            val newIndexesVisited = createIndexes(indexesVisited, index)
-            when (instructions[index].take(3)) {
-                "nop" -> {
-                    runProgram(
-                        instructions = instructions,
-                        index = index + 1,
-                        indexesVisited = newIndexesVisited
-                    )
-                }
-                "jmp" -> {
-                    runProgram(
-                        instructions = instructions,
-                        index = index + instructions[index].substringAfter(" ").toInt(),
-                        indexesVisited = newIndexesVisited
-                    )
-                }
-                "acc" -> {
-                    acc += instructions[index].substringAfter(" ").toInt()
-                    runProgram(
-                        instructions = instructions,
-                        index = index + 1,
-                        newIndexesVisited
-                    )
-                }
-                else -> throw UnknownError("Instruction not understood")
+fun runProgram(
+    instructions: List<String>,
+    index: Int = 0,
+    indexesVisited: List<Int> = emptyList(),
+    acc: Int = 0
+): Int {
+    return if (indexesVisited.contains(index)) {
+        acc
+    } else {
+        val newIndexesVisited = createIndexes(indexesVisited, index)
+        when (instructions[index].take(3)) {
+            "nop" -> {
+                runProgram(
+                    instructions = instructions,
+                    index = index + 1,
+                    indexesVisited = newIndexesVisited,
+                    acc = acc
+                )
             }
+            "jmp" -> {
+                runProgram(
+                    instructions = instructions,
+                    index = index + instructions[index].substringAfter(" ").toInt(),
+                    indexesVisited = newIndexesVisited,
+                    acc = acc
+                )
+            }
+            "acc" -> {
+                val newAcc = accumulate(acc, instructions[index].substringAfter(" ").toInt())
+                runProgram(
+                    instructions = instructions,
+                    index = index + 1,
+                    indexesVisited = newIndexesVisited,
+                    acc = newAcc
+                )
+            }
+            else -> throw UnknownError("Instruction not understood")
         }
     }
-
-    private fun createIndexes(startingList: List<Int> = emptyList(), additionalIndex: Int): List<Int> =
-        startingList + additionalIndex
 }
+
+private fun createIndexes(startingList: List<Int> = emptyList(), additionalIndex: Int): List<Int> =
+    startingList + additionalIndex
+
+private fun accumulate(startingAcc: Int, accValue: Int): Int =
+    startingAcc + accValue
--- src/test/kotlin/aoc/BootCodeKtTest.kt
+++ src/test/kotlin/aoc/BootCodeKtTest.kt
@@ -7,30 +7,27 @@ import org.junit.jupiter.api.Test
 class BootCodeKtTest {
     @Test
     fun `should execute acc and jmp commands and return 1`() {
-        val device = HandheldDevice()
         val instructions = listOf("nop +5", "acc +1", "jmp -1")
 
-        val result = device.runProgram(instructions)
+        val result = runProgram(instructions)
 
         assertThat(result).isEqualTo(1)
     }
 
     @Test
     fun `should execute instructions and return acc as 5`() {
-        val device = HandheldDevice()
         val instructions = exampleBootCode
 
-        val result = device.runProgram(instructions)
+        val result = runProgram(instructions)
 
         assertThat(result).isEqualTo(5)
     }
 
     @Test
     fun `should execute full input instructions and return acc`() {
-        val device = HandheldDevice()
         val instructions = bootCode
 
-        val result = device.runProgram(instructions)
+        val result = runProgram(instructions)
 
         assertThat(result).isEqualTo(1548)
     }
