3b72a7ad6dec19a701eac041d738897a88ce455e
Matt Thompson <matt.thompson@johnlewis.co.uk>
2020-12-13 13:34:07 +0000

    Refactor runProgram to return an Output with acc and exit code

--- src/main/kotlin/aoc/BootCode.kt
+++ src/main/kotlin/aoc/BootCode.kt
@@ -1,40 +1,55 @@
 package aoc
 
+// part 1
+
 fun runProgram(
     instructions: List<String>,
     index: Int = 0,
     indexesVisited: List<Int> = emptyList(),
     acc: Int = 0
-): Int {
+): Output {
     return if (indexesVisited.contains(index)) {
-        acc
+        Output(acc, "loop")
     } else {
         val newIndexesVisited = createIndexes(indexesVisited, index)
         when (instructions[index].take(3)) {
             "nop" -> {
-                runProgram(
-                    instructions = instructions,
-                    index = index + 1,
-                    indexesVisited = newIndexesVisited,
-                    acc = acc
-                )
+                if (index == instructions.size - 1) {
+                    Output(acc, "terminated")
+                } else {
+                    runProgram(
+                        instructions = instructions,
+                        index = index + 1,
+                        indexesVisited = newIndexesVisited,
+                        acc = acc
+                    )
+                }
             }
             "jmp" -> {
-                runProgram(
-                    instructions = instructions,
-                    index = index + instructions[index].substringAfter(" ").toInt(),
-                    indexesVisited = newIndexesVisited,
-                    acc = acc
-                )
+                val jumpNumber = instructions[index].substringAfter(" ").toInt()
+                if (index + jumpNumber > instructions.size - 1) {
+                    Output(acc, "terminated")
+                } else {
+                    runProgram(
+                        instructions = instructions,
+                        index = index + jumpNumber,
+                        indexesVisited = newIndexesVisited,
+                        acc = acc
+                    )
+                }
             }
             "acc" -> {
                 val newAcc = accumulate(acc, instructions[index].substringAfter(" ").toInt())
-                runProgram(
-                    instructions = instructions,
-                    index = index + 1,
-                    indexesVisited = newIndexesVisited,
-                    acc = newAcc
-                )
+                if (index == instructions.size - 1) {
+                    Output(newAcc, "terminated")
+                } else {
+                    runProgram(
+                        instructions = instructions,
+                        index = index + 1,
+                        indexesVisited = newIndexesVisited,
+                        acc = newAcc
+                    )
+                }
             }
             else -> throw UnknownError("Instruction not understood")
         }
@@ -46,3 +61,48 @@ private fun createIndexes(startingList: List<Int> = emptyList(), additionalIndex
 
 private fun accumulate(startingAcc: Int, accValue: Int): Int =
     startingAcc + accValue
+
+// part 2
+
+/*
+Change one nop to jmp or jmp to nop at a time
+if the program runs as expected in part one above that change was incorrect - revert
+
+If the program reaches the end (outOfBounds or .length + 1) return acc - this was correct
+
+*/
+
+fun checkInstruction(instructions: List<String>, index: Int = 0): Boolean {
+    val instruction = instructions[index].take(3)
+    return if (instruction == "nop" || instruction == "jmp") {
+        runProgram(createNewInstructions(index, instructions)).acc < 0
+    } else {
+        false
+    }
+}
+
+fun createNewInstructions(index: Int, instructions: List<String>): List<String> =
+    when (instructions[index].take(3)) {
+        "nop" -> instructions.mapIndexed { instructionIndex, instruction ->
+            if (instructionIndex == index) {
+                "jmp${instruction.substringAfter("nop")}"
+            } else {
+                instruction
+            }
+        }
+
+        "jmp" -> instructions.mapIndexed { instructionIndex, instruction ->
+            if (instructionIndex == index) {
+                "nop${instruction.substringAfter("jmp")}"
+            } else {
+                instruction
+            }
+        }
+
+        else -> instructions
+    }
+
+data class Output(
+    val acc: Int,
+    val exitCode: String
+)
--- src/test/kotlin/aoc/BootCodeKtTest.kt
+++ src/test/kotlin/aoc/BootCodeKtTest.kt
@@ -2,16 +2,19 @@ package aoc
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isFalse
 import org.junit.jupiter.api.Test
 
 class BootCodeKtTest {
+    // part 1
+
     @Test
     fun `should execute acc and jmp commands and return 1`() {
         val instructions = listOf("nop +5", "acc +1", "jmp -1")
 
         val result = runProgram(instructions)
 
-        assertThat(result).isEqualTo(1)
+        assertThat(result.acc).isEqualTo(1)
     }
 
     @Test
@@ -20,7 +23,7 @@ class BootCodeKtTest {
 
         val result = runProgram(instructions)
 
-        assertThat(result).isEqualTo(5)
+        assertThat(result.acc).isEqualTo(5)
     }
 
     @Test
@@ -29,6 +32,73 @@ class BootCodeKtTest {
 
         val result = runProgram(instructions)
 
-        assertThat(result).isEqualTo(1548)
+        assertThat(result.acc).isEqualTo(1548)
+    }
+
+    // part 2
+
+    @Test
+    fun `should return new instructions`() {
+        val instructions = exampleBootCode
+        val index = 0
+
+        val result = createNewInstructions(index, instructions)
+
+        val expected = listOf(
+            "jmp +0",
+            "acc +1",
+            "jmp +4",
+            "acc +3",
+            "jmp -3",
+            "acc -99",
+            "acc +1",
+            "jmp -4",
+            "acc +6"
+        )
+
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should loop and return acc of zero`() {
+        val instructions = exampleBootCode
+        val index = 0
+
+        val result = checkInstruction(instructions, index)
+
+        assertThat(result).isFalse()
+    }
+
+    /*
+    runProgram needs to return a Pair<Int, String> for acc and exitCode
+    - i.e. Pair(4, "terminated") or Pair(45, "loop")
+    */
+
+    @Test
+    fun `should return acc of 5 and exit code of loop from program`() {
+        val instructions = exampleBootCode
+
+        val result = runProgram(instructions)
+
+        assertThat(result).isEqualTo(Output(5, "loop"))
+    }
+
+    @Test
+    fun `should return acc of 8 and exit code of terminated from program`() {
+        val instructions = listOf(
+            "nop +0",
+            "acc +1",
+            "jmp +4",
+            "acc +3",
+            "jmp -3",
+            "acc -99",
+            "acc +1",
+            "nop -4",
+            "acc +6"
+        )
+
+        val result = runProgram(instructions)
+
+        assertThat(result).isEqualTo(Output(8, "terminated"))
     }
 }
