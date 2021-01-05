b3834968cfb5d59cdcfba4628fc9e8390a4e54ca
Matt Thompson <matt.thompson@johnlewis.co.uk>
2021-01-05 19:16:27 +0000

    Add day 16 part 2

--- src/main/kotlin/aoc/TicketDetails.kt
+++ src/main/kotlin/aoc/TicketDetails.kt
@@ -1,6 +1,6 @@
 package aoc
 
-fun invalidValues(input: String): Int {
+fun errorRate(input: String): Int {
     val validation = input.substringBefore("\n\n").split("\n")
     val tickets = input.substringAfter("nearby tickets:\n").split("\n")
 
@@ -17,6 +17,93 @@ fun invalidValues(input: String): Int {
             }
         }
     }.flatten().reduce { total, next -> total + next }
+}
+
+// Part 2
+
+fun multiplyDepartures(input: String, start: String): Long {
+    val fieldOrder = fieldOrder(input)
+    val departureIndexes = fieldOrder.mapIndexedNotNull { index, field ->
+        if (field.contains(start)) index else null
+    }
+
+    val ticketValues = input
+        .substringAfter("your ticket:\n")
+        .substringBefore("\n\nnearby tickets:")
+        .split(",")
+        .map { it.toLong() }
+        .filterIndexed { index, _ -> departureIndexes.contains(index) }
+
+    return ticketValues.reduce { total, next -> total * next }
+}
+
+fun fieldOrder(input: String): List<String> {
+    val validNearbyTickets = validTickets(input)
+    val validations = input.substringBefore("\n\n").split("\n")
+    val fieldsWithValidation = fieldsAndRanges(validations)
+    val ticketsAndFieldValues = validNearbyTickets.map { ticket ->
+        ticket.split(",").map { it.toInt() }
+    }
+
+    val fields = mutableListOf<Pair<Int, String>>()
+    for (index in ticketsAndFieldValues[0].indices) {
+        val fieldValuesAtIndex = ticketsAndFieldValues.map { it[index] }
+
+        val validFields = fieldsWithValidation.filter {
+            Pair(it.second, it.third).containsAll(fieldValuesAtIndex)
+        }
+
+        fields.addAll(validFields.map { Pair(index, it.first) })
+    }
+
+    val uniqueFields = findUniqueFields(fields)
+
+    return uniqueFields.sortedBy { it.first }.map { it.second }
+}
+
+fun validTickets(input: String): List<String> {
+    val validation = input.substringBefore("\n\n").split("\n")
+    val tickets = input.substringAfter("nearby tickets:\n").split("\n")
+
+    val fieldsWithValidation = fieldsAndRanges(validation)
+
+    val ticketsAndFieldValues = tickets.map { ticket ->
+        ticket.split(",").map { it.toInt() }
+    }
+
+    return ticketsAndFieldValues.filter { values ->
+        values.all { value ->
+            fieldsWithValidation.any {
+                it.second.contains(value) || it.third.contains(value)
+            }
+        }
+    }.map { it.joinToString(",") }
+}
+
+// helpers
+
+private fun Pair<IntRange, IntRange>.containsAll(values: List<Int>): Boolean =
+    values.all {
+        this.first.contains(it) || this.second.contains(it)
+    }
+
+private fun findUniqueFields(fields: List<Pair<Int, String>>): List<Pair<Int, String>> {
+    val startingFields = fields.toMutableList()
+    val uniqueFields = mutableListOf<Pair<Int, String>>()
+
+    while (uniqueFields.size < fields.distinctBy { it.second }.size) {
+        val fieldCount = startingFields.groupingBy { it.second }.eachCount()
+        val singleOccurrenceFields = fieldCount.filter { it.value == 1 }.keys.toList()
+
+        singleOccurrenceFields.map { singleOccurrenceFieldName ->
+            val field = startingFields.single { it.second == singleOccurrenceFieldName }
+            uniqueFields.add(field)
+            startingFields.remove(field)
+            startingFields.removeAll(fields.filter { it.first == field.first })
+        }
+    }
+
+    return uniqueFields
 }
 
 private fun fieldsAndRanges(validation: List<String>): List<FieldAndRanges> =
@@ -37,12 +124,3 @@ private fun fieldsAndRanges(validation: List<String>): List<FieldAndRanges> =
     }
 
 typealias FieldAndRanges = Triple<String, IntRange, IntRange>
-
-
-/*
-maybe in part 2?
-
-1. find ticketFieldValues that are only valid for one fieldWithValidation
-2. Remove those ticketValues and those fieldsWithValidation
-3. Then keep looping with shorter lists so that others are only valid for one field (and remove those)
- */
--- src/test/kotlin/aoc/TicketDetailsKtTest.kt
+++ src/test/kotlin/aoc/TicketDetailsKtTest.kt
@@ -1,6 +1,7 @@
 package aoc
 
 import assertk.assertThat
+import assertk.assertions.containsExactly
 import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
@@ -19,7 +20,7 @@ class TicketDetailsKtTest {
             40,4,50
         """.trimIndent()
 
-        val result = invalidValues(input)
+        val result = errorRate(input)
 
         assertThat(result).isEqualTo(4)
     }
@@ -28,7 +29,7 @@ class TicketDetailsKtTest {
     fun `should return invalid ticket scanning error rate of 71 for full example string input`() {
         val input = exampleTicketDetails
 
-        val result = invalidValues(input)
+        val result = errorRate(input)
 
         assertThat(result).isEqualTo(71)
     }
@@ -37,9 +38,73 @@ class TicketDetailsKtTest {
     fun `should return invalid ticket scanning error rate for full input`() {
         val input = ticketDetails
 
-        val result = invalidValues(input)
+        val result = errorRate(input)
         println(result)
 
         assertThat(result).isEqualTo(22073)
+    }
+
+    // Part 2
+
+    @Test
+    fun `should discard invalid tickets`() {
+        val input = exampleTicketDetails
+
+        val result = validTickets(input)
+
+        assertThat(result).isEqualTo(listOf("7,3,47"))
+    }
+
+    @Test
+    fun `should return field order for simple example`() {
+        val input = """
+            class: 0-1 or 4-19
+            row: 0-5 or 8-19
+            seat: 0-13 or 16-19
+
+            your ticket:
+            11,12,13
+
+            nearby tickets:
+            3,9,18
+            15,1,5
+            5,14,9
+        """.trimIndent()
+
+        val result = fieldOrder(input)
+
+        assertThat(result).isEqualTo(listOf("row","class","seat"))
+    }
+
+    @Test
+    fun `should multiply values of fields at indexes which start with c in my ticket`() {
+        val input = """
+            class: 0-1 or 4-19
+            row: 0-5 or 8-19
+            cushion: 0-13 or 16-19
+
+            your ticket:
+            11,12,13
+
+            nearby tickets:
+            3,9,18
+            15,1,5
+            5,14,9
+        """.trimIndent()
+
+        val expected = 12*13L
+        val result = multiplyDepartures(input, "c")
+
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should multiply values of fields at indexes which start with departure in my full input`() {
+        val input = ticketDetails
+
+        val result = multiplyDepartures(input, "departure")
+        println(result)
+
+        assertThat(result).isEqualTo(1346570764607)
     }
 }
