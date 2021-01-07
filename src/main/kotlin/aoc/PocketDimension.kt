48889c046a902561cb7dd711d888c0acbcb7dfe2
Matt Thompson <matt.thompson@johnlewis.co.uk>
2021-01-07 20:18:59 +0000

    Get neighbouring cubes with status for day 17 part 1

--- src/main/kotlin/aoc/PocketDimension.kt
+++ src/main/kotlin/aoc/PocketDimension.kt
@@ -11,19 +11,34 @@ val initialState = listOf(
     "#.##.#.#"
 )
 
-typealias CubeCoord = Triple<Int, Int, Int>
-// first = row, second = column, third = z-plane
+typealias CubeCoord = Triple<Int, Int, Int> // first = row, second = column, third = z-plane
+typealias Neighbour = Pair<Char, CubeCoord>  // char is status (active = # | inactive = .)
 
-fun getNeighbours(cube: CubeCoord): List<CubeCoord> {
+fun getNeighboursWithState(cube: CubeCoord, input: List<String>): List<Neighbour> {
     val rowRange = cube.first - 1..cube.first + 1
     val colRange = cube.second - 1..cube.second + 1
     val zRange = cube.third - 1..cube.third + 1
 
-    return rowRange.flatMap { rowCoord ->
+    val neighbourCoords = rowRange.flatMap { rowCoord ->
         colRange.flatMap { colCoord ->
             zRange.map { zCoord ->
                 CubeCoord(rowCoord, colCoord, zCoord)
             }
         }
     }.filterNot { it == cube }
+
+    val inputCoords = getInputCoords(input)
+
+    return neighbourCoords.map {
+        if (inputCoords.contains(it)) {
+            Neighbour(input[it.first][it.second], it)
+        } else Neighbour('.', it)
+    }
 }
+
+private fun getInputCoords(input: List<String>): List<CubeCoord> =
+    input.flatMapIndexed { rowIndex, row ->
+        row.mapIndexed { colIndex, _ ->
+            CubeCoord(rowIndex, colIndex, 0)
+        }
+    }
--- src/test/kotlin/aoc/PocketDimensionKtTest.kt
+++ src/test/kotlin/aoc/PocketDimensionKtTest.kt
@@ -2,43 +2,47 @@ package aoc
 
 import assertk.assertThat
 import assertk.assertions.containsOnly
-import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
 class PocketDimensionKtTest {
     @Test
-    fun `should return neighbouring cubes`() {
+    fun `should return neighbouring cubes with status`() {
+        val input = listOf(
+            ".#.", // (0,1,0) - self, so not a neighbour
+            "..#", // (1,2,0)
+            "###"  // (2,0,0), (2,1,0), (2,2,0) - all outside of neighbouring cubes, so not returned
+        )
         val cube = CubeCoord(0,1,0)
 
-        val result = getNeighbours(cube)
+        val result = getNeighboursWithState(cube, input)
 
         assertThat(result).containsOnly(
-            CubeCoord(-1, 0, -1),
-            CubeCoord(-1, 1, -1),
-            CubeCoord(-1, 2, -1),
-            CubeCoord(0, 0, -1),
-            CubeCoord(0, 1, -1),
-            CubeCoord(0, 2, -1),
-            CubeCoord(1, 0, -1),
-            CubeCoord(1, 1, -1),
-            CubeCoord(1, 2, -1),
-            CubeCoord(-1, 0, 0),
-            CubeCoord(-1, 1, 0),
-            CubeCoord(-1, 2, 0),
-            CubeCoord(0, 0, 0),
-            CubeCoord(0, 2, 0),
-            CubeCoord(1, 0, 0),
-            CubeCoord(1, 1, 0),
-            CubeCoord(1, 2, 0),
-            CubeCoord(-1, 0, 1),
-            CubeCoord(-1, 1, 1),
-            CubeCoord(-1, 2, 1),
-            CubeCoord(0, 0, 1),
-            CubeCoord(0, 1, 1),
-            CubeCoord(0, 2, 1),
-            CubeCoord(1, 0, 1),
-            CubeCoord(1, 1, 1),
-            CubeCoord(1, 2, 1)
+            Neighbour('.', CubeCoord(-1, 0, -1)),
+            Neighbour('.', CubeCoord(-1, 1, -1)),
+            Neighbour('.', CubeCoord(-1, 2, -1)),
+            Neighbour('.', CubeCoord(0, 0, -1)),
+            Neighbour('.', CubeCoord(0, 1, -1)),
+            Neighbour('.', CubeCoord(0, 2, -1)),
+            Neighbour('.', CubeCoord(1, 0, -1)),
+            Neighbour('.', CubeCoord(1, 1, -1)),
+            Neighbour('.', CubeCoord(1, 2, -1)),
+            Neighbour('.', CubeCoord(-1, 0, 0)),
+            Neighbour('.', CubeCoord(-1, 1, 0)),
+            Neighbour('.', CubeCoord(-1, 2, 0)),
+            Neighbour('.', CubeCoord(0, 0, 0)),
+            Neighbour('.', CubeCoord(0, 2, 0)),
+            Neighbour('.', CubeCoord(1, 0, 0)),
+            Neighbour('.', CubeCoord(1, 1, 0)),
+            Neighbour('#', CubeCoord(1, 2, 0)),
+            Neighbour('.', CubeCoord(-1, 0, 1)),
+            Neighbour('.', CubeCoord(-1, 1, 1)),
+            Neighbour('.', CubeCoord(-1, 2, 1)),
+            Neighbour('.', CubeCoord(0, 0, 1)),
+            Neighbour('.', CubeCoord(0, 1, 1)),
+            Neighbour('.', CubeCoord(0, 2, 1)),
+            Neighbour('.', CubeCoord(1, 0, 1)),
+            Neighbour('.', CubeCoord(1, 1, 1)),
+            Neighbour('.', CubeCoord(1, 2, 1))
         )
     }
 }
