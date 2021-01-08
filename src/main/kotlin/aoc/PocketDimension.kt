49cee428739fb9938b75a3933d9c91c75af74748
Matt Thompson <matt.thompson@johnlewis.co.uk>
2021-01-08 17:56:22 +0000

    Make input 3-dimensional for day 17 part 1

--- src/main/kotlin/aoc/PocketDimension.kt
+++ src/main/kotlin/aoc/PocketDimension.kt
@@ -1,44 +1,66 @@
 package aoc
 
 val initialState = listOf(
-    "##..#.#.",
-    "###.#.##",
-    "..###..#",
-    ".#....##",
-    ".#..####",
-    "#####...",
-    "#######.",
-    "#.##.#.#"
+    listOf(
+        "##..#.#.",
+        "###.#.##",
+        "..###..#",
+        ".#....##",
+        ".#..####",
+        "#####...",
+        "#######.",
+        "#.##.#.#"
+    )
 )
 
-typealias CubeCoord = Triple<Int, Int, Int> // first = row, second = column, third = z-plane
+typealias CubeCoord = Triple<Int, Int, Int> // first = zPlane, second = row, third = column
 typealias Neighbour = Pair<Char, CubeCoord>  // char is status (active = # | inactive = .)
 
-fun getNeighboursWithState(cube: CubeCoord, input: List<String>): List<Neighbour> {
-    val rowRange = cube.first - 1..cube.first + 1
-    val colRange = cube.second - 1..cube.second + 1
-    val zRange = cube.third - 1..cube.third + 1
+//fun cycleState(input: List<List<String>>): Int {
+//    val inputCoords = getInputCoords(input)
+//
+//    inputCoords.map { cubeCoord ->
+//        val neighbours = getNeighboursWithState(cubeCoord, input, inputCoords)
+//        val status = input[cubeCoord.first][cubeCoord.second][cubeCoord.third]
+//        val numberOfActiveNeighbours = neighbours.map { it.first }.filter { it == '#' }.size
+//        when {
+//            status == '#' && numberOfActiveNeighbours == 2 || numberOfActiveNeighbours == 3 -> 0
+//            else -> 1
+//        }
+//    }
+//
+//    return 0
+//}
 
-    val neighbourCoords = rowRange.flatMap { rowCoord ->
-        colRange.flatMap { colCoord ->
-            zRange.map { zCoord ->
-                CubeCoord(rowCoord, colCoord, zCoord)
+fun getNeighboursWithState(
+    cube: CubeCoord,
+    input: List<List<String>>,
+    inputCoords: List<CubeCoord> = getInputCoords(input)
+): List<Neighbour> {
+    val zRange = cube.first - 1..cube.first + 1
+    val rowRange = cube.second - 1..cube.second + 1
+    val colRange = cube.third - 1..cube.third + 1
+
+    val neighbourCoords = zRange.flatMap { zCoord ->
+        rowRange.flatMap { rowCoord ->
+            colRange.map { colCoord ->
+                CubeCoord(zCoord, rowCoord, colCoord)
             }
         }
     }.filterNot { it == cube }
 
-    val inputCoords = getInputCoords(input)
-
     return neighbourCoords.map {
         if (inputCoords.contains(it)) {
-            Neighbour(input[it.first][it.second], it)
+            Neighbour(input[it.first][it.second][it.third], it)
         } else Neighbour('.', it)
     }
 }
 
-private fun getInputCoords(input: List<String>): List<CubeCoord> =
-    input.flatMapIndexed { rowIndex, row ->
-        row.mapIndexed { colIndex, _ ->
-            CubeCoord(rowIndex, colIndex, 0)
+private fun getInputCoords(input: List<List<String>>): List<CubeCoord> =
+    input.flatMapIndexed { zIndex, zPlane ->
+        zPlane.flatMapIndexed { rowIndex, row ->
+            row.mapIndexed { colIndex, _ ->
+                CubeCoord(zIndex, rowIndex, colIndex)
+            }
         }
     }
--- src/test/kotlin/aoc/PocketDimensionKtTest.kt
+++ src/test/kotlin/aoc/PocketDimensionKtTest.kt
@@ -2,47 +2,63 @@ package aoc
 
 import assertk.assertThat
 import assertk.assertions.containsOnly
+import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
 class PocketDimensionKtTest {
     @Test
     fun `should return neighbouring cubes with status`() {
         val input = listOf(
-            ".#.", // (0,1,0) - self, so not a neighbour
-            "..#", // (1,2,0)
-            "###"  // (2,0,0), (2,1,0), (2,2,0) - all outside of neighbouring cubes, so not returned
+            listOf(
+                ".#.", // (0,0,1) - self, so not a neighbour
+                "..#", // (0,1,2)
+                "###"  // (0,2,0), (0,2,1), (0,2,2) - all outside of neighbouring cubes, so not returned
+            )
         )
-        val cube = CubeCoord(0,1,0)
+        val cube = CubeCoord(0, 0, 1)
 
         val result = getNeighboursWithState(cube, input)
 
         assertThat(result).containsOnly(
-            Neighbour('.', CubeCoord(-1, 0, -1)),
-            Neighbour('.', CubeCoord(-1, 1, -1)),
-            Neighbour('.', CubeCoord(-1, 2, -1)),
-            Neighbour('.', CubeCoord(0, 0, -1)),
-            Neighbour('.', CubeCoord(0, 1, -1)),
-            Neighbour('.', CubeCoord(0, 2, -1)),
-            Neighbour('.', CubeCoord(1, 0, -1)),
-            Neighbour('.', CubeCoord(1, 1, -1)),
-            Neighbour('.', CubeCoord(1, 2, -1)),
+            Neighbour('.', CubeCoord(-1, -1, 0)),
+            Neighbour('.', CubeCoord(-1, -1, 1)),
+            Neighbour('.', CubeCoord(-1, -1, 2)),
             Neighbour('.', CubeCoord(-1, 0, 0)),
-            Neighbour('.', CubeCoord(-1, 1, 0)),
-            Neighbour('.', CubeCoord(-1, 2, 0)),
-            Neighbour('.', CubeCoord(0, 0, 0)),
-            Neighbour('.', CubeCoord(0, 2, 0)),
-            Neighbour('.', CubeCoord(1, 0, 0)),
-            Neighbour('.', CubeCoord(1, 1, 0)),
-            Neighbour('#', CubeCoord(1, 2, 0)),
             Neighbour('.', CubeCoord(-1, 0, 1)),
+            Neighbour('.', CubeCoord(-1, 0, 2)),
+            Neighbour('.', CubeCoord(-1, 1, 0)),
             Neighbour('.', CubeCoord(-1, 1, 1)),
-            Neighbour('.', CubeCoord(-1, 2, 1)),
-            Neighbour('.', CubeCoord(0, 0, 1)),
+            Neighbour('.', CubeCoord(-1, 1, 2)),
+            Neighbour('.', CubeCoord(0, -1, 0)),
+            Neighbour('.', CubeCoord(0, -1, 1)),
+            Neighbour('.', CubeCoord(0, -1, 2)),
+            Neighbour('.', CubeCoord(0, 0, 0)),
+            Neighbour('.', CubeCoord(0, 0, 2)),
+            Neighbour('.', CubeCoord(0, 1, 0)),
             Neighbour('.', CubeCoord(0, 1, 1)),
-            Neighbour('.', CubeCoord(0, 2, 1)),
+            Neighbour('#', CubeCoord(0, 1, 2)),
+            Neighbour('.', CubeCoord(1, -1, 0)),
+            Neighbour('.', CubeCoord(1, -1, 1)),
+            Neighbour('.', CubeCoord(1, -1, 2)),
+            Neighbour('.', CubeCoord(1, 0, 0)),
             Neighbour('.', CubeCoord(1, 0, 1)),
+            Neighbour('.', CubeCoord(1, 0, 2)),
+            Neighbour('.', CubeCoord(1, 1, 0)),
             Neighbour('.', CubeCoord(1, 1, 1)),
-            Neighbour('.', CubeCoord(1, 2, 1))
+            Neighbour('.', CubeCoord(1, 1, 2))
         )
     }
+
+//    @Test
+//    fun `should return 11 active cubes after single state cycle`() {
+//        val input = listOf(
+//            listOf(".#."),
+//            listOf("..#"),
+//            listOf("###")
+//        )
+//
+//        val result = cycleState(input)
+//
+//        assertThat(result).isEqualTo(11)
+//    }
 }
